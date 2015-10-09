938b775b43c27c4f1493f3c56f926dbe64fbdebe
篠崎 清智 <shinozakikana@yahoo.co.jp>
2015-10-09 12:32:45 +0900

    病院デザイン設定初期コミット

---
+++ app/Controller/TopPagesController.php
@@ -0,0 +1,11 @@
+<?php
+class TopPagesController extends AppController{
+	
+	public $uses = array('sample');
+
+    public function index(){
+    	
+    	var_dump($this->sample);
+        $this->set('samples',$this->sample->find('all'));
+    }
+}
